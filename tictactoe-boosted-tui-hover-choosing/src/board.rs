4104fbaebb2fb599d70a687bbab99f0064a71e24
5a-lil <khalilhadjmes1@gmail.com>
2026-08-25 12:25:19 +0200

    .

--- tictactoe-boosted-tui-hover-choosing/src/board.rs
+++ tictactoe-boosted-tui-hover-choosing/src/board.rs
@@ -6,9 +6,10 @@ use ratatui::{
 
 use crossterm::event::{self, KeyCode};
 
-pub const BOARD_SIZE: u16 = 19;
-pub const HOR_SIZE: u16 = 4;
-pub const VER_SIZE: u16 = 2;
+const BOARD_SIZE: u16 = 19;
+const HOR_SIZE: u16 = 4;
+const VER_SIZE: u16 = 2;
+const WIN_COND: u16 = 5;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum CellState {
@@ -44,16 +45,144 @@ impl Board {
     }
 
     pub fn handle_mouse_move(&mut self, col: u16, row: u16) {
+        // Temporarily blocking play when there is a win or draw
+        // if self._playing == CellState::Empty {
+        //     return;
+        // }
+
         self._mouse_position = (col, row)
     }
 
     pub fn handle_mouse_left_click(&mut self) {
+        // Temporarily blocking play when there is a win or draw
+        if self._playing == CellState::Empty {
+            return;
+        }
+
         for (i, a) in self._board_areas.iter().enumerate() {
             if a.contains(Position::new(self._mouse_position.0, self._mouse_position.1)) {
                 self._board_states[i] = self.play();
+                self.check_game(i);
                 return;
             }
         }
+    }
+
+    fn check_game(&mut self, played_index: usize) {
+        let played: CellState = self._board_states[played_index];
+
+        // horizontal
+        let mut hor_count: u16 = 0;
+        {
+            let mut left: usize = played_index;
+            while self._board_states[left] == played {
+                hor_count += 1;
+                if left as u16 % self._cols == 0 {
+                    break;
+                }
+                left -= 1;
+            }
+
+            let mut right: usize = played_index;
+            while self._board_states[right] == played {
+                hor_count += 1;
+                if (right as u16 + 1) % self._cols == 0 {
+                    break;
+                }
+                right += 1;
+            }
+
+            if hor_count - 1 >= WIN_COND {
+                self._playing = CellState::Empty;
+                return
+            }
+        }
+
+        // vertical
+        let mut ver_count: u16 = 0;
+        {
+            let mut up: usize = played_index;
+            while self._board_states[up] == played {
+                ver_count += 1;
+                if (up as i16 - self._cols as i16) < 0 {
+                    break;
+                }
+                up -= self._cols as usize;
+            }
+
+            let mut down: usize = played_index;
+            while self._board_states[down] == played {
+                ver_count += 1;
+                if down as u16 + self._cols >= self._cols * self._cols {
+                    break;
+                }
+                down += self._cols as usize;
+            }
+
+            if ver_count - 1 >= WIN_COND {
+                self._playing = CellState::Empty;
+                return
+            }
+        }
+
+        // up left going diags
+        let mut up_left_diag_count: u16 = 0;
+        {
+            let mut up: usize = played_index;
+            while self._board_states[up] == played {
+                up_left_diag_count += 1;
+                let futur_move = up as i16 - self._cols as i16 - 1;
+                if futur_move < 0 || up % self._cols as usize == 0 {
+                    break;
+                }
+                up = futur_move as usize;
+            }
+
+            let mut down: usize = played_index;
+            while self._board_states[down] == played {
+                up_left_diag_count += 1;
+                let futur_move = down as u16 + self._cols + 1;
+                if futur_move >= self._cols * self._cols || (down + 1) % self._cols as usize == 0 {
+                    break;
+                }
+                down = futur_move as usize;
+            }
+
+            if up_left_diag_count - 1 >= WIN_COND {
+                self._playing = CellState::Empty;
+                return
+            }
+        }
+
+        // down right going diags
+        let mut up_right_diag_count: u16 = 0;
+        {
+            let mut up: usize = played_index;
+            while self._board_states[up] == played {
+                up_right_diag_count += 1;
+                let futur_move = up as i16 - self._cols as i16 + 1;
+                if futur_move < 0 || (up + 1) % self._cols as usize == 0 {
+                    break;
+                }
+                up = futur_move as usize;
+            }
+
+            let mut down: usize = played_index;
+            while self._board_states[down] == played {
+                up_right_diag_count += 1;
+                let futur_move = down as u16 + self._cols - 1;
+                if futur_move >= self._cols * self._cols || down % self._cols as usize == 0 {
+                    break;
+                }
+                down = futur_move as usize;
+            }
+
+            if up_right_diag_count - 1 >= WIN_COND {
+                self._playing = CellState::Empty;
+                return
+            }
+        }
+
     }
 
     fn play(&mut self) -> CellState {
